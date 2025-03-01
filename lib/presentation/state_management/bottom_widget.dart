8980790042241155931a0679c17c9ba9436812dd
mdtayobali13 <mdtayobalideveloper@gmail.com>
2025-03-01 10:31:12 +0600

    update

---
+++ lib/presentation/state_management/bottom_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomWidget extends StatefulWidget {
+  const BottomWidget({super.key});
+
+  @override
+  State<BottomWidget> createState() => _BottomWidgetState();
+}
+
+class _BottomWidgetState extends State<BottomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
